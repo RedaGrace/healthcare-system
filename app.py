14125b5febcf04670fc8cb18a3c6fcbccc677fb5
Reda Grace <41922411+RedaGrace@users.noreply.github.com>
2024-06-04 18:48:03 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import *
+import pandas as pd
+import json, time
+import pickle
+import re
+import os
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods = ['GET'])
+def home_page():
+    return render_template('index.html')
+
+if __name__=='__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
